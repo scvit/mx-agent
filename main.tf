a42084c88947a28648384ee99e6c3f95488fcc5c
scvit <88419463+scvit@users.noreply.github.com>
2025-04-15 10:42:46 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-/*
+
 provider "azurerm" {
   features {}
 
@@ -12,14 +12,14 @@ provider "azurerm" {
   client_id = var.client1_id
 
 }
-*/
+
 
 provider "azurerm" {
   features {}
 
   # Managed Identity로 인증
   use_msi = true
-  # alias = "managed-2"
+   alias = "managed-2"
   # 구독 ID 명시 (필수 아님, 환경에 따라 필요)
   subscription_id = var.sub_id
   tenant_id = var.tenant_id
@@ -34,8 +34,9 @@ variable "tenant_id" {}
 variable "client1_id" {}
 variable "client2_id" {}
 
+/*
 data "azurerm_resource_group" "rg" {
-  # provider = azurerm.managed-2
+   provider = azurerm.managed-2
 
   name = "mw-resource-group-2" # "dwwon-rg-1" 
 }
@@ -43,9 +44,9 @@ data "azurerm_resource_group" "rg" {
 output "id" {
   value = data.azurerm_resource_group.rg.id
 }
+*/
 
 
-/*
  resource "azurerm_virtual_network" "rg2_vpc" {
     provider = azurerm.managed-2
    name                = "mwjo-network-rg2"
@@ -53,4 +54,4 @@ output "id" {
    location            = data.azurerm_resource_group.rg.location
    resource_group_name = "mw-resource-group-2"
  }
-*/
+
