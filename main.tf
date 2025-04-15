55beb8b0a3a645d5278fcabf7c1721fddee86114
scvit <88419463+scvit@users.noreply.github.com>
2025-04-15 14:21:47 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ variable "tenant_id" {}
 variable "client1_id" {}
 variable "client2_id" {}
 
-/*
+
 data "azurerm_resource_group" "rg" {
    provider = azurerm.managed-2
 
@@ -44,9 +44,9 @@ data "azurerm_resource_group" "rg" {
 output "id" {
   value = data.azurerm_resource_group.rg.id
 }
-*/
 
 
+/*
  resource "azurerm_virtual_network" "rg2_vpc" {
     provider = azurerm.managed-2
    name                = "mwjo-network-rg2"
@@ -54,4 +54,4 @@ output "id" {
    location            = "Korea Central"
    resource_group_name = "mw-resource-group-2"
  }
-
+*/
